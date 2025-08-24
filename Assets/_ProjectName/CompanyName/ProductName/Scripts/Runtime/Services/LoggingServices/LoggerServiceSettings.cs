22bec9569048088b86d487698d33249967453719
Juri <knauth.juri@gmail.com>
2025-08-24 14:15:58 +0200

    Renamed Settings

--- Assets/_ProjectName/CompanyName/ProductName/Scripts/Runtime/Services/LoggingServices/LoggerServiceSettings.cs
+++ Assets/_ProjectName/CompanyName/ProductName/Scripts/Runtime/Services/LoggingServices/LoggerServiceSettings.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 
 namespace CompanyName.ProductName.Scripts.Runtime.Services.LoggingServices
 {
-    public sealed class SettingsSectionLoggerService : SettingsSection
+    public sealed class LoggerServiceSettings : SettingsSection
     {
-        [field: Header("LoggerService")]
         [field: SerializeField]
         public LogLevel MinimumLogLevel { get; private set; } = LogLevel.Info;
     }
